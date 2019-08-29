3caa0096040f4cf0f249dd84b5bdc5e076d25393
Strukov20 <32924439+Strukov20@users.noreply.github.com>
2019-08-29 13:47:51 +0300

    adaptive1

---
+++ script.js
@@ -0,0 +1,295 @@
+html {
+	font-size: 12px;
+}
+@media only screen and (max-width:1024px) {
+	html {
+		font-size: 9.375px;
+		width: 750px;
+		
+	}
+}
+
+}
+
+	#main-nav-wrap {
+	font-family: "raleway-heavy", sans-serif;
+	font-size: 12px;
+	position: absolute;
+	text-transform: uppercase;
+	letter-spacing: 1.5px;
+	
+	top: 0;
+	right: 190px;
+}
+
+	.logo {
+	padding-top: 59px;
+	padding-left: 60px;
+	width: 91px;
+}
+.container {
+	line-height: normal;
+	padding-top: 280px;
+	text-align:center;
+	color: white;
+	font-family: Raleway;
+}
+
+h1 {
+	font-size: 36px;	
+	text-transform: uppercase;
+}
+h3 {
+	font-size: 12px;
+	}
+
+
+	.button{
+	background: #00e6b8;
+	color: white;
+	border-radius:5px;
+	display: block;
+	width:120px;
+	padding: 20px 0;
+	margin: 0 auto;
+	text-decoration:none;
+	text-align: center;
+	text-transform: uppercase;
+	font-weight: bold;
+	font-size: 12px;
+	margin-top: 100px;
+
+}
+
+.slogan_item {
+	margin-bottom: 60px;
+
+
+}
+
+.slogan {
+	max-width: 280px;
+	width: 100%;
+	float: left;
+	text-align: center;
+
+	
+}
+
+.slogan h2 {
+	font-size: 12px;
+	margin-bottom: 5px;
+}
+
+.slogan p {
+	color: #8c8c8c;
+	font-size: 8px;
+}
+
+.slogan:last-child  {
+	padding-top: 62px;
+	float: right;
+	margin-right: 40px;
+
+}
+
+.slogan:nth-child(3)  {
+	padding-top: 56px;
+	float: right;
+	margin-right: 80px;
+}
+
+.slogan:nth-child(2)  {
+	padding-top: 52px;
+	float: left;
+	margin-left: 40px;
+
+}
+
+.slogan:nth-child(1)  {
+	padding-top: 50px;
+	float: left;
+	margin-left: 80px;
+}
+
+.a123123{
+
+	  background-color: #fbfaf8;
+      height: 290px;
+      
+      top: 0;
+      left: 0;
+      width: 100%;
+
+}
+
+.img1{
+	width: 20px;
+
+}
+
+.a123124{
+
+	  background-color: #372720;
+      height: 150px;
+      
+      top: 0;
+      left: 0;
+      width: 100%;
+
+     
+}
+
+.section4 {
+	padding-top: 40px;
+	margin-left: 70px;
+	color:white;
+	float:left;
+	  
+}
+
+.button2 {
+	background: #998675;
+	color: white;
+	border-radius:5px;
+	display: block;
+	width:100px;
+	padding: 15px 0;
+	margin: 50px 100px auto;
+	text-decoration:none;
+	text-align: center;
+	text-transform: uppercase;
+	font-weight: bold;
+	font-size: 10px;
+	float: 	right;	      
+	  
+} 
+
+
+.our-s {
+	max-width: 750px;
+	width: 100%;
+	padding-top: 40px;
+	text-align: center;
+}
+
+.our {
+	text-align: center;
+	font-size: 24px;
+}
+
+.our-p {
+	text-align: center;
+	font-size: 12px;
+}
+
+.all-button {
+	max-width: 900px;
+	width: 100%;
+	padding-top: 30px;
+	padding-bottom: 60px;	
+	padding-left: 215px;
+
+}
+
+.button3 {
+	max-width: 50px;
+	width: 100%;
+	float: left;
+	text-align: center;
+	text-decoration: none;
+	font-size: 8px;
+	background: #ffffff;
+	color: #8c8c8c;
+	display: block;
+	margin-left: 10px;
+	padding: 10px 0;
+	border-radius:4px;
+
+	border: 1px solid #ebebeb;
+
+	
+}
+
+.button4 {
+	max-width: 30px;
+	width: 100%;
+	float: left;
+	text-align: center;
+	text-decoration: none;
+	font-size: 8px;
+	background: #998675;
+	color: #ffffff;
+	display: block;
+	margin-left: 10px;
+	padding: 10px 0;
+	border-radius:4px;
+
+	border: 1px solid #ebebeb;
+
+	
+}
+
+.photo6{
+	max-width: 600px;
+	width: 100%;
+	padding-left: 75px;
+
+}
+
+.button5 {
+	background: #998675;
+	color: white;
+	border-radius:5px;
+	display: block;
+	width:200px;
+	padding: 18px 0;
+	margin: 0 auto;
+	text-decoration:none;
+	text-align: center;
+	text-transform: uppercase;
+	font-weight: bold;
+	font-size: 16px;
+	margin-top: 50px;
+	margin-bottom: 80px;
+		
+}
+
+
+.videoplay {
+	background: url(img/icon-play.png) ;
+	height: 504px;
+	max-width: 750px;
+	width: 100%;
+	text-align: center;
+}
+
+.video {
+	text-align: center;
+	color: #ededed;
+	padding-top: 150px;
+	font-size: 16px;
+}
+
+.timer {
+ padding-top: 50px;
+ color: #c2ad95;
+
+}
+@media only screen and (max-width:768px) {
+	html {
+		font-size: 10px;
+	}
+}
+@media only screen and (max-width:400px) {
+	html {
+		font-size: 9.375px;
+	}
+}
+
+@media screen and (max-width: 1920px) {
+
+	body {
+		width: 100%;
+	}
+}
